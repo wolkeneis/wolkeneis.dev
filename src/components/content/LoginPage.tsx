1a0ba27e77c8dc2b773373eaf3675010f73398ef
Simon <simon@benezan.de>
2022-05-13 08:24:05 +0200

    google login

--- src/components/content/LoginPage.tsx
+++ src/components/content/LoginPage.tsx
@@ -1,5 +1,39 @@
+import { Google as GoogleIcon } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia
+} from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 const LoginPage = () => {
-  return <></>;
+  return (
+    <Box
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Card sx={{ margin: "5px", width: "345px" }}>
+        <CardHeader avatar={<GoogleIcon />} title="Google" />
+        <CardMedia
+          alt="Random Image"
+          component="img"
+          height="154px"
+          image={`https://picsum.photos/seed/${uuidv4()}/600`}
+        />
+        <CardActions>
+          <Button
+            component="a"
+            href={`${
+              process.env.REACT_APP_MOOS_BACKEND ?? "https://moos.wolkeneis.dev"
+            }/login/google`}
+          >
+            Login
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
 };
 
 export default LoginPage;
