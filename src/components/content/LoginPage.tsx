2e18bdb606cf1038a1c7b6791b8370978e099bf0
Simon <simon@benezan.de>
2022-05-13 20:57:41 +0200

    Update LoginPage.tsx

--- src/components/content/LoginPage.tsx
+++ src/components/content/LoginPage.tsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         margin: "auto"
       }}
     >
-      <Card sx={{ width: "345px" }}>
+      <Card sx={{ width: "345px", marginTop: "-35%" }}>
         <CardHeader title="Login" />
         <CardMedia
           alt="Random Image"
