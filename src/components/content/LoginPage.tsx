17e97d3d7e6281dbc372bc3d907d5f5108626270
Simon <simon@benezan.de>
2022-05-13 11:40:23 +0200

    add discord login

--- src/components/content/LoginPage.tsx
+++ src/components/content/LoginPage.tsx
@@ -8,10 +8,21 @@ import {
   CardMedia
 } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
+import DarkDiscordIcon from "../../media/discord-dark.svg";
+import LightDiscordIcon from "../../media/discord-light.svg";
+import { useAppSelector } from "../../redux/hooks";
+
 const LoginPage = () => {
+  const theme = useAppSelector((state) => state.interface.theme);
+
   return (
     <Box
-      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
     >
       <Card sx={{ margin: "5px", width: "345px" }}>
         <CardHeader avatar={<GoogleIcon />} title="Google" />
@@ -27,6 +38,37 @@ const LoginPage = () => {
             href={`${
               process.env.REACT_APP_MOOS_BACKEND ?? "https://moos.wolkeneis.dev"
             }/login/google`}
+          >
+            Login
+          </Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ margin: "5px", width: "345px" }}>
+        <CardHeader
+          avatar={
+            <Box sx={{ width: "24px", height: "24px" }}>
+              <img
+                src={
+                  theme === "light-theme" ? DarkDiscordIcon : LightDiscordIcon
+                }
+                width="24px"
+              />
+            </Box>
+          }
+          title="Discord"
+        />
+        <CardMedia
+          alt="Random Image"
+          component="img"
+          height="154px"
+          image={`https://picsum.photos/seed/${uuidv4()}/600`}
+        />
+        <CardActions>
+          <Button
+            component="a"
+            href={`${
+              process.env.REACT_APP_MOOS_BACKEND ?? "https://moos.wolkeneis.dev"
+            }/login/discord`}
           >
             Login
           </Button>
