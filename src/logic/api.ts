3df3d8c57146d8077cc9fc9221e95ddcfbcb7f0f
Simon <simon@benezan.de>
2022-07-01 11:54:03 +0200

    Update api.ts

--- src/logic/api.ts
+++ src/logic/api.ts
@@ -2,6 +2,70 @@ import { type v1 } from "moos-api";
 import { setCSRFToken } from "../redux/sessionSlice";
 import { store } from "../redux/store";
 
+export async function createCollection(
+  options: v1.operations["put-profile-collection"]["requestBody"]["content"]["application/json"]
+): Promise<v1.SeasonGroup | null> {
+  return (
+    ((await _csrfFetch("/profile/collection", options, { method: "PUT" }).then(
+      (response) => response.json()
+    )) as v1.SeasonGroup) ?? null
+  );
+}
+
+export async function createSeason(
+  options: v1.operations["put-profile-list"]["requestBody"]["content"]["application/json"]
+): Promise<v1.Season | null> {
+  return (
+    ((await _csrfFetch("/profile/list", options, { method: "PUT" }).then(
+      (response) => response.json()
+    )) as v1.Season) ?? null
+  );
+}
+
+export async function createEpisode(
+  options: v1.operations["put-profile-episode"]["requestBody"]["content"]["application/json"]
+): Promise<v1.Episode | null> {
+  return (
+    ((await _csrfFetch("/profile/episode", options, { method: "PUT" }).then(
+      (response) => response.json()
+    )) as v1.Episode) ?? null
+  );
+}
+
+export async function createSource(
+  options: v1.operations["put-profile-source"]["requestBody"]["content"]["application/json"]
+): Promise<v1.Source | null> {
+  return (
+    ((await _csrfFetch("/profile/source", options, { method: "PUT" }).then(
+      (response) => response.json()
+    )) as v1.Source) ?? null
+  );
+}
+
+export async function createFile(
+  options: v1.operations["put-profile-file"]["requestBody"]["content"]["application/json"]
+): Promise<
+  | v1.operations["put-profile-file"]["responses"]["200"]["content"]["application/json"]
+  | null
+> {
+  return (
+    ((await _csrfFetch("/profile/file", options, { method: "PUT" }).then(
+      (response) => response.json()
+    )) as v1.operations["put-profile-file"]["responses"]["200"]["content"]["application/json"]) ??
+    null
+  );
+}
+
+export async function deleteFile(
+  options: v1.operations["delete-profile-file"]["requestBody"]["content"]["application/json"]
+): Promise<boolean> {
+  return (
+    (await _csrfFetch("/profile/file", options, { method: "DELETE" }).then(
+      (response) => response.ok
+    )) ?? false
+  );
+}
+
 export async function fetchProfile(): Promise<v1.UserProfile | null> {
   return _csrfFetch("/profile", undefined, { method: "POST" }).then(
     (response) => response.json()
