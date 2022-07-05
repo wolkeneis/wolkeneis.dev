a2e94b09500d61b202fd1e74d653ab6c92ffd2c8
Simon <simon@benezan.de>
2022-07-05 22:11:18 +0200

    file upload working

--- src/components/content/Files.tsx
+++ src/components/content/Files.tsx
@@ -8,8 +8,6 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Input,
-  TextField,
   Typography
 } from "@mui/material";
 import {
@@ -21,8 +19,9 @@ import {
   GridToolbarExport
 } from "@mui/x-data-grid";
 import { v1 } from "moos-api";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { createFile, deleteFile } from "../../logic/api";
 import { updateFileList } from "../../logic/files";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { setUploadDialogVisible } from "../../redux/interfaceSlice";
@@ -163,10 +162,10 @@ const Grid = ({ root }: { root: Directory }) => {
 
   return (
     <>
-      <UploadDialog />
+      <UploadDialog currentDirectory={currentDirectory} />
       {gridData && (
         <DataGrid
-          columns={[{ field: "name", headerName: "Name" }]}
+          columns={[{ field: "name", headerName: "Name", flex: 2 }]}
           components={{
             NoRowsOverlay: CustomNoRowsOverlay,
             Toolbar: CustomToolbar
@@ -202,7 +201,13 @@ const CustomToolbar = () => {
   );
 };
 
-const UploadDialog = () => {
+const UploadDialog = ({
+  currentDirectory
+}: {
+  currentDirectory: Directory;
+}) => {
+  const [file, setFile] = useState<globalThis.File>();
+  const fileInput = useRef<HTMLInputElement | null>(null);
   const uploadDialogVisible = useAppSelector(
     (state) => state.interface.uploadDialogVisible
   );
@@ -212,17 +217,88 @@ const UploadDialog = () => {
     dispatch(setUploadDialogVisible(false));
   };
 
+  const handleFileSelection = () => {
+    if (fileInput.current?.files) {
+      const file = fileInput.current.files[0];
+      if (file) {
+        setFile(file);
+        console.log(file);
+      }
+    }
+  };
+
+  const handleUpload = async () => {
+    handleClose();
+    if (file) {
+      const response = await createFile({
+        name: `${
+          currentDirectory
+            ? currentDirectory === root
+              ? ""
+              : currentDirectory.id
+            : ""
+        }${file.name}`
+      });
+      if (response) {
+        try {
+          const abortController = new AbortController();
+          setTimeout(() => abortController.abort(), response.ttl);
+          await fetch(response.url, {
+            method: "PUT",
+            signal: abortController.signal,
+            body: file
+          });
+          setFile(undefined);
+        } catch (error) {
+          await deleteFile({ id: response.id });
+        }
+      }
+    }
+  };
+
   return (
     <div>
       <Dialog onClose={handleClose} open={uploadDialogVisible}>
         <DialogTitle>File Upload</DialogTitle>
         <DialogContent>
-          <DialogContentText>Please select a File to upload.</DialogContentText>
-          <Input type="file"></Input>
+          {file ? (
+            <DialogContentText
+              sx={{
+                width: "12rem",
+                overflow: "hidden",
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis"
+              }}
+            >
+              {file.name}
+            </DialogContentText>
+          ) : (
+            <DialogContentText>
+              Please select a File to upload.
+            </DialogContentText>
+          )}
+          <input
+            id="file"
+            name="file"
+            onChange={handleFileSelection}
+            ref={fileInput}
+            style={{ display: "none" }}
+            type="file"
+          ></input>
+          <label htmlFor="file">
+            <Button
+              component="div"
+              startIcon={<UploadFileIcon />}
+              sx={{ marginY: 1.5 }}
+              variant="outlined"
+            >
+              Select File
+            </Button>
+          </label>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Upload</Button>
+          <Button onClick={handleUpload}>Upload</Button>
         </DialogActions>
       </Dialog>
     </div>
